b020bc25bf963676c8d70d9ae3c882cf1cf33ef3
Alexander Leitner <leitnersalex@gmail.com>
2018-05-16 15:28:59 +0900

    Added seek, readAt, and writeAt

--- fpiece.go
+++ fpiece.go
@@ -1,6 +1,7 @@
 package fpiece // import "github.com/aleitner/FilePiece"
 
 import (
+	"errors"
 	"io"
 	"os"
 )
@@ -12,6 +13,12 @@ type Chunk struct {
 	Final      int64
 	CurrentPos int64
 }
+
+const (
+	SeekStart   = 0 // seek relative to the origin of the file
+	SeekCurrent = 1 // seek relative to the current offset
+	SeekEnd     = 2 // seek relative to the end
+)
 
 // Create Chunk
 func NewChunk(file *os.File, offset int64, length int64) *Chunk {
@@ -40,6 +47,24 @@ func (f *Chunk) Read(b []byte) (n int, err error) {
 	return n, err
 }
 
+func (f *Chunk) ReadAt(p []byte, off int64) (n int, err error) {
+	if off < 0 || off >= f.Final-f.Start {
+		return 0, io.EOF
+	}
+
+	off += f.Start
+
+	if max := f.Final - off; int64(len(p)) > max {
+		p = p[0:max]
+		n, err = f.File.ReadAt(p, off)
+		if err == nil {
+			err = io.EOF
+		}
+		return n, err
+	}
+	return f.File.ReadAt(p, off)
+}
+
 // Concurrently write to Chunk
 func (f *Chunk) Write(b []byte) (n int, err error) {
 	if f.CurrentPos >= f.Final {
@@ -56,4 +81,46 @@ func (f *Chunk) Write(b []byte) (n int, err error) {
 	n, err = f.File.WriteAt(b[:writeLen], f.CurrentPos)
 	f.CurrentPos += int64(n)
 	return n, err
+}
+
+func (f *Chunk) WriteAt(p []byte, off int64) (n int, err error) {
+	if off < 0 || off >= f.Final-f.Start {
+		return 0, io.EOF
+	}
+
+	off += f.Start
+
+	if max := f.Final - off; int64(len(p)) > max {
+		p = p[0:max]
+		n, err = f.File.WriteAt(p, off)
+		if err == nil {
+			err = io.EOF
+		}
+		return n, err
+	}
+	return f.File.WriteAt(p, off)
+}
+
+var errWhence = errors.New("Seek: invalid whence")
+var errOffset = errors.New("Seek: invalid offset")
+
+func (f *Chunk) Seek(offset int64, whence int) (int64, error) {
+	switch whence {
+	default:
+		return 0, errWhence
+	case SeekStart:
+		offset += f.Start
+	case SeekCurrent:
+		offset += f.CurrentPos
+	case SeekEnd:
+		offset += f.Final
+	}
+
+	// Do not seek to where somewhere outside the chunk
+	if offset < f.Start || offset > f.Final {
+		return 0, errOffset
+	}
+
+	f.CurrentPos = offset
+	return offset - f.Start, nil
 }
--- fpiece_test.go
+++ fpiece_test.go
@@ -21,6 +21,18 @@ var readTests = []struct {
 	{"butts", 1, 1100, "utts"},
 }
 
+var readAtTests = []struct {
+	in     string
+	offset int64
+	out    string
+}{
+	{"butts", 0, "butts"},
+	{"butts", 1, "utts"},
+	{"butts", 2, "tts"},
+	{"butts", 100, ""},
+	{"butts", -1, ""},
+}
+
 var writeTests = []struct {
 	in     string
 	offset int64
@@ -32,6 +44,18 @@ var writeTests = []struct {
 	{"butts", 3, 2, "\x00\x00\x00bu"},
 	{"butts", 0, 10, "butts"},
 	{"butts", 1, 1100, "\x00butts"},
+}
+
+var writeAtTests = []struct {
+	in     string
+	offset int64
+	out    string
+}{
+	{"butts", 0, "butts"},
+	{"butts", 1, "\x00butt"},
+	{"butts", 3, "\x00\x00\x00bu"},
+	{"butts", 1000, ""},
+	{"butts", -11, ""},
 }
 
 func TestRead(t *testing.T) {
@@ -68,6 +92,40 @@ func TestRead(t *testing.T) {
 
 }
 
+func TestReadAt(t *testing.T) {
+
+	for _, tt := range readAtTests {
+		t.Run("Reads data properly using ReadAt", func(t *testing.T) {
+
+			tmpfilePtr, err := ioutil.TempFile("", "readAt_test")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			defer os.Remove(tmpfilePtr.Name()) // clean up
+
+			if _, err := tmpfilePtr.Write([]byte(tt.in)); err != nil {
+				log.Fatal(err)
+			}
+
+			chunk := NewChunk(tmpfilePtr, 0, int64(len(tt.in)))
+
+			buffer := make([]byte, 100)
+			n, _ := chunk.ReadAt(buffer, tt.offset)
+
+			if err := tmpfilePtr.Close(); err != nil {
+				log.Fatal(err)
+			}
+
+			if string(buffer[:n]) != tt.out {
+				t.Errorf("got %q, want %q", string(buffer[:n]), tt.out)
+			}
+
+		})
+	}
+
+}
+
 func TestWrite(t *testing.T) {
 
 	for _, tt := range writeTests {
@@ -82,6 +140,37 @@ func TestWrite(t *testing.T) {
 
 			chunk := NewChunk(tmpfilePtr, tt.offset, tt.len)
 			chunk.Write([]byte(tt.in))
+
+			buffer := make([]byte, 100)
+			n, err := tmpfilePtr.Read(buffer)
+
+			if err := tmpfilePtr.Close(); err != nil {
+				log.Fatal(err)
+			}
+
+			if string(buffer[:n]) != tt.out {
+				t.Errorf("got %q, want %q", string(buffer[:n]), tt.out)
+			}
+
+		})
+	}
+
+}
+
+func TestWriteAt(t *testing.T) {
+
+	for _, tt := range writeAtTests {
+		t.Run("Writes data properly", func(t *testing.T) {
+
+			tmpfilePtr, err := ioutil.TempFile("", "writeAt_test")
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			defer os.Remove(tmpfilePtr.Name()) // clean up
+
+			chunk := NewChunk(tmpfilePtr, 0, int64(len(tt.in)))
+			chunk.WriteAt([]byte(tt.in), tt.offset)
 
 			buffer := make([]byte, 100)
 			n, err := tmpfilePtr.Read(buffer)
